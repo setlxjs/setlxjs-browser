f376c72284ce96099368e2d90df417f63db0de34
hendrikniemann <h.neverman@web.de>
2016-07-31 15:09:35 +0200

    improve syntax highlight

--- src/setlxLanguage.js
+++ src/setlxLanguage.js
@@ -1,28 +1,27 @@
-/* Example definition of a simple mode that understands a subset of
- * JavaScript:
- */
+import { stdlib } from 'setlxjs-transpiler/build/plugins/StdLibPlugin';
+
+const std = RegExp('(?:' + stdlib.join('|') + '|procedure)\\b');
 
 CodeMirror.defineSimpleMode('setlx', {
   // The start state contains the rules that are intially used
   start: [
     {regex: /"(?:[^\\]|\\.)*?"/, token: "string"},
-    {regex: /(procedure)(\s+)([a-z$][\w$]*)/, token: ["keyword", null, "variable-2"]},
     // Rules are matched in the order in which they appear, so there is
     // no ambiguity between this one and the one above
-    {regex: /(?:function|var|return|if|for|while|else|do|this|forall|exists)\b/,
+    {regex: /(?:return|if|for|while|else|forall|exists)\b/,
      token: "keyword"},
-    {regex: /true|false|om/, token: "atom"},
+    {regex: /true|false|om|omega/, token: "atom"},
+    {regex: std, token: "builtin"},
     {regex: /0x[a-f\d]+|[-+]?(?:\.\d+|\d+\.?\d*)(?:e[-+]?\d+)?/i,
      token: "number"},
     {regex: /\/\/.*/, token: "comment"},
-    {regex: /\/(?:[^\\]|\\.)*?\//, token: "variable-3"},
     // A next property will cause the mode to move to a different state
     {regex: /\/\*/, token: "comment", next: "comment"},
-    {regex: /[-+\/*=<>!]+|in/, token: "operator"},
+    {regex: /in|:=|[-+\/*=<>!]+/, token: "operator"},
     // indent and dedent properties guide autoindentation
     {regex: /[\{\[\(]/, indent: true},
     {regex: /[\}\]\)]/, dedent: true},
-    {regex: /[a-z$][\w$]*/, token: "variable"},
+    {regex: /[a-z][a-zA-Z1-9]*/, token: "variable"},
   ],
   // The multi-line comment state.
   comment: [
